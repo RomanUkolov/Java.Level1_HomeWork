87b7d3bca0947e2d737e079d85efa302ff6d1e67
RomanUkolov <Roman_tora@mail.ru>
2022-02-12 13:06:51 +0300

    сделан 1пункт дз

--- HomeWork_2.java
+++ HomeWork_2.java
@@ -1,6 +1,17 @@
+/**
+ * Java_Level 1 HomeWork_2
+ * 
+ * @autor Roman Ukolov
+ * @version 12.02.2022
+ */
+ 
 class HomeWork_2 {
     public static void main (String[] args) {
-
-
+        boolean result = CheckNumberSum (11, 8);
+        System.out.println(result);
+    }
+    
+    static boolean CheckNumberSum (int a, int b) {
+        return a + b >=10 && a + b <= 20;
     }
 }
