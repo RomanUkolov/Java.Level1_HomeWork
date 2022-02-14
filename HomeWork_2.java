ec6ced10b203c8229bfac18310f1837441783367
RomanUkolov <88205067+RomanUkolov@users.noreply.github.com>
2022-02-14 20:21:34 +0300

    Update HomeWork_2.java

--- HomeWork_2.java
+++ HomeWork_2.java
@@ -7,27 +7,27 @@
  
 class HomeWork_2 {
     public static void main (String[] args) {
-        boolean result = CheckNumberSum (11, 8);
+        boolean result = checkNumberSum (11, 8);
         System.out.println("Check - " + result);
-        PositiveOrNegative (-10);
-        boolean res2 = NegativeNumber (-5);
+        positiveOrNegative (-10);
+        boolean res2 = negativeNumber (-5);
         System.out.println("Result - " + res2);
-        RepeatWord ("Hello", 3);
+        repeatWord ("Hello", 3);
     }
     
-    static boolean CheckNumberSum (int a, int b) {
+    static boolean checkNumberSum (int a, int b) {
         return a + b >= 10 && a + b <= 20;
     }
     
-    static void PositiveOrNegative ( int i) {
+    static void positiveOrNegative ( int i) {
         System.out.println(i >= 0? "Number is positive" : "Number is negative");
     }
     
-    static boolean NegativeNumber (int i) {
+    static boolean negativeNumber (int i) {
         return i <= 0;
     }
     
-    static void RepeatWord (String text, int times) {
+    static void repeatWord (String text, int times) {
         for (int i = 0; i < times; i++) {
             System.out.println(text);
         }
